245538018072ad8b89ff49303169324b5003b477
Steven Wade <steven@stevenwade.co.uk>
2016-03-17 12:42:11 +0000

    decreasing the logging to "warn" instead of "info" within chef client.

--- recipes/default.rb
+++ recipes/default.rb
@@ -14,4 +14,5 @@ include_recipe 'chocolatey'
 
 Chef::Log.info 'Installing chef-client and set its interval ...'
 node.override['chef_client']['interval'] = '2'
+node.override['chef_client']['config']['log_level'] = 'warn'
 include_recipe 'chef-client'
