ac80837fd3112538072e1d2cf063663d98b713cb
Steven Wade <steven@stevenwade.co.uk>
2016-04-05 14:38:47 +0100

    Starting to get the cookbook more like the "pds template"

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,11 +4,6 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-# Don't do this - should throw FoodCritic (FC001: Use strings in preference to symbols to access node attributes)
-#package node[:cookbook][:package] do
-#  action :install
-#end
-
 if node['platform'] == 'ubuntu'
   include_recipe 'core::linux'
 end
